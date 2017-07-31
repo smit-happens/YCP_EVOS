f36c5ca952c0c03eb97312f07ec7f144bdd7ffe5
PatHock <phock@ycp.edu>
2017-07-30 23:31:27 -0400

    Added experimental APPS test code.

--- Safety_System_Integration/Safety_System_Integration.ino
+++ Safety_System_Integration/Safety_System_Integration.ino
@@ -7,13 +7,14 @@
  * For now, this is mostly test code. Follows similar structure to the
  * Super Dooper Looper 
  */
-
+#include "TimerOne.h"
 
 #define BSPD_ERR_LED  33
 #define BSPD_IN 23
+#define ACCEL_IN 32
 
 // Super loop variable to track which interrupts have occurred
-uint8_t globalInterruptTracker = 0; 
+uint8_t glblIntTrack = 0; 
 
 
 int main(void)
@@ -21,9 +22,16 @@ int main(void)
   // Setup code. Initializes I/O pins and enables interrupts
   pinMode(BSPD_ERR_LED, OUTPUT);  // Sets pin 33 as an output
   pinMode(BSPD_IN, INPUT);  // Sets pin 23 as input
-  attachInterrupt(digitalPinToInterrupt(BSPD_IN), BSPD_Fault, RISING);
+  attachInterrupt(digitalPinToInterrupt(BSPD_IN), ISR_BSPD_Fault, RISING);
+  int lclIntTrack;
 
-  int localInterruptTracker;
+  // Following is test code for Accelerator pedal position sensing
+  Timer1.initialize(10000);  // Sets a 10ms period for the timer
+  Timer1.attachInterrupt(ISR_ADC_TIMER);
+  int adcValue;
+  
+  Serial.begin(9600); // Starts 9600 baud serial connection for ADC reading
+  delay(500);
   
   // Super dooper looper
   while(1)
@@ -32,33 +40,60 @@ int main(void)
      * All interrupts must be disabled while copying the global interrupt tracker to 
      * the local variable. After interrupts are re-enabled, any interrupts that occur
      * while this loop is executing will be handled on the next iteration of the loop
+     * 
+     * Bit 0 is BSPD error interrupt, bit 1 is ADC timer interrupt
      */
+     
     noInterrupts(); // disables all interrupts
     
-    localInterruptTracker = globalInterruptTracker;
-    globalInterruptTracker = 0;
+    lclIntTrack = glblIntTrack;
+    glblIntTrack = 0;
     
     interrupts();   // re-enables interrupts
 
     
     // Check for BSPD Error
-    if(bitRead(localInterruptTracker, 0))
+    if(bitRead(lclIntTrack, 0))
     {
       digitalWrite(BSPD_ERR_LED, HIGH);
-      bitWrite(localInterruptTracker, 0, 0);    // Resets the "interrupt flag" for BSPD Error
+      bitWrite(lclIntTrack, 0, 0);    // Resets the "interrupt flag" for BSPD Error
     }
+
+
+    // Check for timer pop
+    if(bitRead(lclIntTrack, 1))
+    {
+      adcValue = analogRead(ACCEL_IN);
+      adcValue = 100 * adcValue / 1024;
+      Serial.println(adcValue);
+      bitWrite(lclIntTrack, 1, 0);    // Resets the "interrupt flag" for Timer pop
+    }
+
     
   }
 }
 
-void BSPD_Fault()
+
+void ISR_BSPD_Fault()
 {
   /*
-   * updates bit 0 of the globalInterruptTracker to indicate that a BSPD 
+   * updates bit 0 of the glblIntTrack to indicate that a BSPD 
    * error has occurred
    */
   
-  bitWrite(globalInterruptTracker, 0, 1); // Sets "interrupt flag" for BSPD error
+  bitWrite(glblIntTrack, 0, 1); // Sets "interrupt flag" for BSPD error
 
-  return;
 }
+
+
+
+void ISR_ADC_TIMER()
+{
+  /*
+   * Updates bit 1 of glblIntTrack to indicate that timer has overflowed
+   */
+   
+   bitWrite(glblIntTrack, 1, 1);  // Sets flag for ADC timer
+
+}
+
