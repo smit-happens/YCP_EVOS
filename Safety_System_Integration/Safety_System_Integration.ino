9512f77b8405f55a6b8b397c6abc268988370ed3
PatHock <phock@ycp.edu>
2017-07-30 21:29:47 -0400

    Implemented the structure of the super loop. Also tried to make code more readable.

--- Safety_System_Integration/Safety_System_Integration.ino
+++ Safety_System_Integration/Safety_System_Integration.ino
@@ -4,46 +4,61 @@
  * Patrick Hock
  * July 30 2017
  * 
- * For now, this is mostly test code. 
- * Getting familiar with Arduino.
- * This program will blink an LED external to the Teensy.
+ * For now, this is mostly test code. Follows similar structure to the
+ * Super Dooper Looper 
  */
- 
+
+
 #define BSPD_ERR_LED  33
 #define BSPD_IN 23
-uint8_t interruptTracker = 0;
 
-void setup() {
-  
+// Super loop variable to track which interrupts have occurred
+uint8_t globalInterruptTracker = 0; 
+
+
+int main(void)
+{
+  // Setup code. Initializes I/O pins and enables interrupts
   pinMode(BSPD_ERR_LED, OUTPUT);  // Sets pin 33 as an output
   pinMode(BSPD_IN, INPUT);  // Sets pin 23 as input
-
   attachInterrupt(digitalPinToInterrupt(BSPD_IN), BSPD_Fault, RISING);
+
+  int localInterruptTracker;
   
-}
+  // Super dooper looper
+  while(1)
+  {
+    /*
+     * All interrupts must be disabled while copying the global interrupt tracker to 
+     * the local variable. After interrupts are re-enabled, any interrupts that occur
+     * while this loop is executing will be handled on the next iteration of the loop
+     */
+    noInterrupts(); // disables all interrupts
+    
+    localInterruptTracker = globalInterruptTracker;
+    globalInterruptTracker = 0;
+    
+    interrupts();   // re-enables interrupts
 
-void loop() {
- 
-// Check for BSPD Error
-if(bitRead(interruptTracker, 0))
-{
-  digitalWrite(BSPD_ERR_LED, HIGH);
-  bitWrite(interruptTracker, 0, 0);    // Resets the "interrupt flag" for BSPD Error
-}
-
+    
+    // Check for BSPD Error
+    if(bitRead(localInterruptTracker, 0))
+    {
+      digitalWrite(BSPD_ERR_LED, HIGH);
+      bitWrite(localInterruptTracker, 0, 0);    // Resets the "interrupt flag" for BSPD Error
+    }
+    
+  }
 }
 
 void BSPD_Fault()
 {
-  noInterrupts();
+  /*
+   * updates bit 0 of the globalInterruptTracker to indicate that a BSPD 
+   * error has occurred
+   */
+  
+  bitWrite(globalInterruptTracker, 0, 1); // Sets "interrupt flag" for BSPD error
 
-  // changes bit 0 to a one so that Loop() can handle the error
-  
-  bitWrite(interruptTracker, 0, 1); // Sets "interrupt flag" for BSPD error
-  
-  interrupts();
-  
   return;
 }
-
-
