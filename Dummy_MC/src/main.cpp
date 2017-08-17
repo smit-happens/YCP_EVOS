583720930166fa6d5865dc9ffd486c9d89aa1cbd
smit-happens <asmit147@ycp.edu>
2017-08-17 16:45:54 -0400

    Some logic changes

--- Dummy_MC/src/main.cpp
+++ Dummy_MC/src/main.cpp
@@ -1,8 +1,7 @@
-#include <Arduino.h>
-
 //Created by Smitty :D
 //8-16-17
 
+#include <Arduino.h>
 #include <FlexCAN.h>
 #include <MCRegIDs.h>
 #include <LiquidCrystalFast.h>
@@ -23,24 +22,33 @@ LiquidCrystalFast lcd(33, 34, 35, 36, 37, 38, 39);
 
 int main(void)
 {
-  Serial.begin(9600);
+  // Serial.begin(9600);
   lcd.begin(20, 4);
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(encoderPinA, INPUT);
   pinMode(encoderPinB, INPUT);
 
-  // encoder pin on interrupt 0 (pin 2)
+  // encoder pinA interrupt
   attachInterrupt(encoderPinA, doEncoderA, CHANGE);
-  // encoder pin on interrupt 1 (pin 3)
+  // encoder pinB interrupt
   attachInterrupt(encoderPinB, doEncoderB, CHANGE);
 
   digitalWrite(LED_BUILTIN, 1);
   lcd.clear();
 
   int cw = 0, ccw = 0;
-  int up = 0, down = 0;
-  bool change = false;
+  int up = 1, down = 1;
+  bool change = true;
+
+  lcd.setCursor(0, 0);
+  lcd.print("cw=  ");
+  lcd.setCursor(9, 0);
+  lcd.print("goUp=  ");
+  lcd.setCursor(0, 1);
+  lcd.print("ccw= ");
+  lcd.setCursor(9, 1);
+  lcd.print("goDwn= ");
 
   while(1) {
     // Serial.println(REG_READ);
@@ -54,33 +62,36 @@ int main(void)
       cw = 0;
       change = true;
     }
-    else {
+    else
       change = false;
-    }
 
+    //doesn't excecute if encoder hasn't turned
     if(change == true) {
       encoderPos = 0;
-      // lcd.clear();
+
+      //notches are at every 4 steps on the encoder
+      //checks every 4 steps to move up/down a menu item
+      if(cw >= 4) {
+        cw = 0;
+        ccw = 0;
+        lcd.setCursor(9, 0);
+        lcd.print("goUp=  ");
+        lcd.print(up++);
+      }
+      else if(ccw >= 4) {
+        ccw = 0;
+        cw = 0;
+        lcd.setCursor(9, 1);
+        lcd.print("goDwn= ");
+        lcd.print(down++);
+      }
+
       lcd.setCursor(0, 0);
       lcd.print("cw=  ");
       lcd.println(cw);
       lcd.print("ccw= ");
       lcd.print(ccw);
-
-      if(cw >= 4) {
-        cw = 0;
-        lcd.setCursor(9, 0);
-        lcd.print("goUp=  ");
-        lcd.print(up++);
-      }
-      else if(ccw >= 4){
-        ccw = 0;
-        lcd.setCursor(9, 1);
-        lcd.print("goDwn= ");
-        lcd.print(down++);
-      }
     }
-
   }
   return 0;
 }
