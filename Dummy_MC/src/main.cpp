8068ce521f61d65f75186e98528d1593da67efde
smit-happens <asmit147@ycp.edu>
2017-08-18 12:31:43 -0400

    Added a moving cursor that you can move up/down with the encoder

--- Dummy_MC/src/main.cpp
+++ Dummy_MC/src/main.cpp
@@ -5,20 +5,36 @@
 #include <FlexCAN.h>
 #include <MCRegIDs.h>
 #include <LiquidCrystalFast.h>
+#include <array>
+#include <string>
+using namespace std;
 
 #define encoderPinA  17
 #define encoderPinB  18
+#define lcdHeight 4
 
 volatile int8_t encoderPos = 0;
 
 boolean A_set = false;
 boolean B_set = false;
 
+LiquidCrystalFast lcd(33, 34, 35, 36, 37, 38, 39);
+//LiquidCrystalFast lcd(RS, RW, Enable, D4, D5, D6, D7);
+
+array <String, 8> menuMain {
+  "Smit item1",
+  "Smit item2",
+  "Smit item3",
+  "Smit item4",
+  "Smit item5",
+  "Smit item6",
+  "Smit item7",
+  "Smit item8",
+};
+
 void doEncoderA(void);
 void doEncoderB(void);
 
-LiquidCrystalFast lcd(33, 34, 35, 36, 37, 38, 39);
-//LiquidCrystalFast lcd(RS, RW, Enable, D4, D5, D6, D7);
 
 int main(void)
 {
@@ -38,67 +54,83 @@ int main(void)
   lcd.clear();
 
   int cw = 0, ccw = 0;
-  int up = 1, down = 1;
-  bool change = true;
+  int index = 0;
+  bool change = false;
 
-  lcd.setCursor(0, 0);
-  lcd.print("cw=  ");
-  lcd.setCursor(9, 0);
-  lcd.print("goUp=  ");
-  lcd.setCursor(0, 1);
-  lcd.print("ccw= ");
-  lcd.setCursor(9, 1);
-  lcd.print("goDwn= ");
+  //displaying initial menu so lcd doesn't start up blank
+  for(uint16_t i = 0; i < lcdHeight; i++) {
+    lcd.setCursor(0, i);
 
-  while(1) {
+    //the user display cursor logic
+    if(i == index)
+      lcd.print(">");
+    else
+      lcd.print(" ");
+
+    lcd.print(i);
+    lcd.print(" ");
+    lcd.print(menuMain[i]);
+  }
+
+  while(1)
+  {
     // Serial.println(REG_READ);
-    if (0 > encoderPos) {
+    if (0 > encoderPos)
+    {
       cw++;
       ccw = 0;
       change = true;
     }
-    else if(0 < encoderPos) {
+    else if(0 < encoderPos)
+    {
       ccw++;
       cw = 0;
       change = true;
     }
-    else
-      change = false;
 
     //doesn't excecute if encoder hasn't turned
-    if(change == true) {
+    if(change == true)
+    {
+      //reset values so this condition isn't executed repeatedly
       encoderPos = 0;
+      change = false;
 
       //notches are at every 4 steps on the encoder
       //checks every 4 steps to move up/down a menu item
-      if(cw >= 4) {
+      if(cw >= 4)
+      {
         cw = 0;
-        ccw = 0;
-        lcd.setCursor(9, 0);
-        lcd.print("goUp=  ");
-        lcd.print(up++);
+        index--;
       }
-      else if(ccw >= 4) {
+      else if(ccw >= 4)
+      {
         ccw = 0;
-        cw = 0;
-        lcd.setCursor(9, 1);
-        lcd.print("goDwn= ");
-        lcd.print(down++);
+        index++;
       }
 
-      lcd.setCursor(0, 0);
-      lcd.print("cw=  ");
-      lcd.println(cw);
-      lcd.print("ccw= ");
-      lcd.print(ccw);
+      //updating the cursor position on the display
+      for(uint16_t i = 0; i < lcdHeight; i++) {
+        lcd.setCursor(0, i);
+
+        //the user display cursor logic
+        if(i == index)
+          lcd.print(">");
+        else
+          lcd.print(" ");
+
+        lcd.print(i);
+        lcd.print(" ");
+        lcd.print(menuMain[i]);
+      }
     }
-  }
+  } //end while(1) loop
   return 0;
 }
 
 
 // Interrupt on A changing state
-void doEncoderA() {
+void doEncoderA()
+{
   // Test transition
   A_set = digitalRead(encoderPinA) == HIGH;
   // and adjust counter + if A leads B
@@ -107,7 +139,8 @@ void doEncoderA() {
 
 
 // Interrupt on B changing state
-void doEncoderB() {
+void doEncoderB()
+{
   // Test transition
   B_set = digitalRead(encoderPinB) == HIGH;
   // and adjust counter + if B follows A
