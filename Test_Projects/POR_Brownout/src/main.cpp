0463385e40d65459f6f475330ac4ca5b471869fa
PatHock <phock@ycp.edu>
2018-02-22 14:49:23 -0500

    Made a "blink" style program - blink an LED using registers.

--- Test_Projects/POR_Brownout/src/main.cpp
+++ Test_Projects/POR_Brownout/src/main.cpp
@@ -7,6 +7,7 @@
  */
 
 #include <kinetis.h>
+#include <core_pins.h>
 #include <Arduino.h>
 
 
@@ -21,7 +22,7 @@ void resetInit(){
 // POR and LVD are disabled in low power modes
 
 //Disable all power modes other than "Normal Run"
-SMC_PMPROT &=  ~(SMC_PMPROT_ALLS & SMC_PMPROT_AHSRUN & SMC_PMPROT_AVLLS & SMC_PMPROT_AVLP);
+SMC_PMPROT &=  ~(SMC_PMPROT_ALLS | SMC_PMPROT_AHSRUN | SMC_PMPROT_AVLLS | SMC_PMPROT_AVLP);
 
 
 // Enable LVD to trigger a reset when voltage is lower than the set threshold
@@ -44,26 +45,52 @@ PMC_LVDSC2 |= PMC_LVDSC2_LVWIE;
 
 int main(void){
 
-    resetInit();
+    //resetInit();
 
-    Serial.begin(9600);
+    // Serial.begin(9600);
     while (!Serial.available()){
 
     }
 
-    
-
     Serial.print("SMC_PMPROT: ");
+    Serial.print(SMC_PMPROT, BIN);
+
+    SMC_PMPROT &=  ~(SMC_PMPROT_ALLS | SMC_PMPROT_AHSRUN | SMC_PMPROT_AVLLS | SMC_PMPROT_AVLP);
+    //SMC_PMPROT = 0;
+
+    Serial.print("\nSMC_PMPROT: ");
     Serial.print(SMC_PMPROT, BIN);
 
     Serial.print("\nPMC_LVDS1: ");
     Serial.print(PMC_LVDSC1, BIN);
 
-    Serial.print("\nPMC_LVDS2: ");
-    Serial.print(PMC_LVDSC2, BIN);
+    PMC_LVDSC1 |= PMC_LVDSC1_LVDRE;
+
+    Serial.print("\nPMC_LVDS1: ");
+    Serial.print(PMC_LVDSC1, BIN);
+
+    // GPIO Test Code
+    // Pin 23 on Teensy is Port C Pin 2
+
+    SIM_SCGC5 = SIM_SCGC5_PORTC;    // Enable clock to port C
+    PORTC_PCR2 &= ~PORT_PCR_MUX_MASK;
+    PORTC_PCR2 |= PORT_PCR_MUX(1);  // Set Pin 23 (PORT C Pin 2) to GPIO
 
 
+    Serial.print("\nGPIOC_PDDR: ");
+    Serial.print(GPIOC_PDDR, BIN);    
+
+    GPIOC_PDDR |= 1ul << 2;   // Set pin 2 (port c) as output
+
+    Serial.print("\nGPIOC_PDDR: "); 
+    Serial.print(GPIOC_PDDR, BIN);
+
+
+    while(1) {
+    GPIOC_PTOR = 1ul << 2;
+    delay(500); 
     
+    }
 
 }
 
