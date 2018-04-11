8e690f9d2addd05bc2f89c0ba1e0a8656b2b571a
YoungMaker <ledzeppelindrummer@gmail.com>
2018-04-11 00:00:45 -0700

    Completed skeleton implementation of Logger.

--- MainBrain/src/Controller/Logger/Logger.cpp
+++ MainBrain/src/Controller/Logger/Logger.cpp
@@ -8,11 +8,46 @@
 
 #include "Logger.hpp"
 
+//to see if the instance of the class has been initialized yet
+Logger* Logger::_pInstance = NULL; 
 
-/**
- * Logger constructor
- */
-Logger::Logger(void)
+void Logger::init(void)
 {
+    //what do we do here??
+    mNumSubscribers = 0;
+}
 
+Logger* Logger::getInstance()
+{
+    if(!_pInstance)
+    {
+        _pInstance = new Logger();
+    }
+    return _pInstance;
+}
+
+bool Logger::addSubscriber(LogListener* listener)
+{
+    //have we exceeded number of broadcast subscribers
+    if(mNumSubscribers >= MAX_SUBSCRIBERS) {return false;}
+    mSubscribers[mNumSubscribers] = listener;
+    mNumSubscribers++; //adds one to the subscriber count
+    return true;
+}
+
+void Logger::msgPump(char* key, char* message, msg_type type)
+{
+    for(int i=0; i< mNumSubscribers; i++)
+    {
+        mSubscribers[i]->onLogFiled(key, message, type);
+    }
+}
+
+bool Logger::log(char* key, char* message, msg_type type)
+{
+    Serial.print(key); 
+    Serial.print("\t");
+    Serial.print(message);
+    msgPump(key, message, type);
+    return true;
 }
--- MainBrain/src/Controller/Logger/Logger.hpp
+++ MainBrain/src/Controller/Logger/Logger.hpp
@@ -10,42 +10,58 @@
 #define LOGGER_HPP
 
 #include "../../Controller/SdCardController/SdCardController.hpp"
+#include "../../Controller/BaseController/BaseController.hpp"
 
 enum msg_type{
     MSG_LOG,
     MSG_DEBUG,
-    MSG_WARN
+    MSG_WARN,
+    MSG_ERR
 };
 
+class LogListener; //forward declaration of logListener
 
-class Logger
+class Logger: BaseController
 {
 public:
-
+    //maximum number of broadcast subscribers
+    static const int MAX_SUBSCRIBERS = 20;
     ~Logger(void);
 
+    void init(void); //we do need an init to setup the singleton.
+    void poll(void) {}; //no need for poll, logger is updated when log is fired
+
+    void shutdown(void) {}; 
+
+    //singleton getter
     static Logger* getInstance();
-    //public logging functions
-    bool log(char* key, char* message,  msg_type);
+    //logs message 
+    bool log(char* key, char* message,  msg_type type);
+    //adds new subscribers to list 
+    bool addSubscriber(LogListener* listener);
     
 
 private:
     Logger();
     Logger(Logger const&) {};
+    
+    int mNumSubscribers;
+    LogListener* mSubscribers[MAX_SUBSCRIBERS];
+    void msgPump(char* key, char* message,  msg_type type);
 
     static Logger* _pInstance;
     //private logging functions/ variables
 
 };
 
-class LogListener
+class LogListener //interface for broadcast 
 {
 public:
     virtual ~LogListener(void) {};
 
-    virtual void onLogFiled(char* key, char* message,  msg_type) const = 0;
+    virtual void onLogFiled(char* key, char* message,  msg_type type) const = 0;
 private:
-    LogListener() {};
+    LogListener(void) {};
 };
 
 #endif  //LOGGER_HPP
